d9dc0f5456ccd3571b13fb8e59668e1eb967b39c
Oliver Egger <oliver.egger@ahdis.ch>
2021-06-03 09:55:59 +0200

    console example

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Serialization;
+
+namespace dotnetfhirconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello FHIR, creating a patient!");
+
+            var pat = new Patient();
+
+            var id = new Identifier();
+            id.System = "http://hl7.org/fhir/sid/us-ssn";
+            id.Value = "000-12-3456";
+            pat.Identifier.Add(id);
+
+            var name = new HumanName().WithGiven("Christopher").WithGiven("C.H.").AndFamily("Parks");
+            name.Prefix = new string[] { "Mr." };
+            name.Use = HumanName.NameUse.Official;
+
+            var nickname = new HumanName();
+            nickname.Use = HumanName.NameUse.Nickname;
+            nickname.GivenElement.Add(new FhirString("Chris"));
+
+            pat.Name.Add(name);
+            pat.Name.Add(nickname);
+
+            pat.Gender = AdministrativeGender.Male;
+
+            pat.BirthDate = "1983-04-23";
+
+            var birthplace = new Extension();
+            birthplace.Url = "http://hl7.org/fhir/StructureDefinition/birthPlace";
+            birthplace.Value = new Address() { City = "Seattle" };
+            pat.Extension.Add(birthplace);
+
+            var birthtime = new Extension("http://hl7.org/fhir/StructureDefinition/patient-birthTime",
+                                           new FhirDateTime(1983, 4, 23, 7, 44));
+            pat.BirthDateElement.Extension.Add(birthtime);
+
+            var address = new Address()
+            {
+                Line = new string[] { "3300 Washtenaw Avenue, Suite 227" },
+                City = "Ann Arbor",
+                State = "MI",
+                PostalCode = "48104",
+                Country = "USA"
+            };
+            pat.Address.Add(address);
+
+            var contact = new Patient.ContactComponent();
+            contact.Name = new HumanName();
+            contact.Name.Given = new string[] { "Susan" };
+            contact.Name.Family = "Parks";
+            contact.Gender = AdministrativeGender.Female;
+            contact.Relationship.Add(new CodeableConcept("http://hl7.org/fhir/v2/0131", "N"));
+            contact.Telecom.Add(new ContactPoint(ContactPoint.ContactPointSystem.Phone, null, ""));
+            pat.Contact.Add(contact);
+
+
+            var serializer = new FhirXmlSerializer();
+            var xmlText = serializer.SerializeToString(pat);
+
+            Console.WriteLine(xmlText);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Console FHIR R4 Application
+
+1. create a [new project](https://docs.microsoft.com/en-us/dotnet/core/tutorials/with-visual-studio-code)
+
+```
+dotnet new console
+dotnet run
+```
+
+2. install [Firely Docs » Firely .NET SDK » Getting Started](https://docs.fire.ly/projects/Firely-NET-SDK/start.html)
+
+dotnet add package Hl7.Fhir.R4
+
+
+3. Use example code from [Firely](https://docs.fire.ly/projects/Firely-NET-SDK/parsing/poco-parsing.html)
